5bef5787a4a19951f362bcd160987aea39554710
esdras <esdras.ferreira@aluno.uepb.edu.br>
2024-10-01 20:54:31 -0300

    adding postcards

--- scripts/postcards.js
+++ scripts/postcards.js
@@ -1,5 +1,5 @@
 (function hideLoader(){
-    const loaderBulma = document.getElementById("loader-bulma");
-    //loaderBulma.style.display = 'none';
+    const loaderBulma = document.getElementsByClassName("loader-bulma")[0];
+    loaderBulma.style.display = 'none';
     loaderBulma.style.transform = 'scale(0.5)';
 })()
